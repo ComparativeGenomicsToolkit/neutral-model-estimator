d0cca5a180bd221d755bda0e6564de372e0a1018
Joel Armstrong <jcarmstr@ucsc.edu>
2017-05-06 14:25:42 -0700

    Parallelize single-copy filtering
    Getting a lot of segfaults from halSingleCopyExtract, but that doesn't
    seem to be directly related.
    
    At some point the batchsystem choice will need to be passed down from
    whatever the root job will be.

--- neutral_model_estimator/filter.py
+++ neutral_model_estimator/filter.py
@@ -1,20 +1,86 @@
-from subprocess import check_call
+import os
+from collections import namedtuple
+from subprocess import check_call, check_output
 
 import luigi
+from toil.common import Toil
+from toil.job import Job
 
 from ancestral_repeats import GenerateAncestralRepeatsBed
 
+# Toil jobs for the extract-single-copy-regions phase
+
+Chunk = namedtuple('Chunk', 'start length')
+
+def extract_single_copy_regions_parallel(job, hal_file, genome, chunk_size):
+    """Get a BED of single-copy regions from a hal file in parallel.
+
+    Child jobs: extract_single_copy_regions_from_chunk
+    Follow-on jobs: collate_chunks"""
+    # Get the total length of the genome: this is necessary for
+    # chunking the genome
+    stats_lines = [line.split(",") for line in check_output(["halStats", hal_file]).split("\n")]
+    relevant_line = next((fields for fields in stats_lines if fields[0] == genome), None)
+    assert relevant_line is not None, "No line for genome %s found in halStats output" % genome
+    genome_length = int(relevant_line[2])
+    # Chunk it up and send the chunked regions to our children
+    chunks = chunk_genome(genome_length, chunk_size)
+    finished_chunks = [job.addChildJobFn(extract_single_copy_regions_from_chunk, hal_file, genome, chunk).rv() for chunk in chunks]
+    # Return a sorted file collating the children's output
+    return job.addFollowOnJobFn(collate_chunks, finished_chunks).rv()
+
+def chunk_genome(genome_length, chunk_size):
+    """Chunks a genome of length genome_length into Chunks of at most chunk_size length.
+
+    Helper function for extract_single_copy_regions_parallel.
+
+    >>> chunk_genome(10, 3)
+    [Chunk(start=0, length=3), Chunk(start=3, length=3), Chunk(start=6, length=3), Chunk(start=9, length=1)]"""
+    chunks = []
+    for i in range(0, genome_length, chunk_size):
+        chunks.append(Chunk(start=i, length=min(chunk_size, genome_length - i)))
+    return chunks
+
+def extract_single_copy_regions_from_chunk(job, hal_file, genome, chunk):
+    """Return a BED containing the single-copy regions from a single chunk."""
+    bed = job.fileStore.getLocalTempFile()
+    with open(bed, 'w') as f:
+        check_call(["halSingleCopyRegionsExtract", hal_file, genome, "--start", str(chunk.start),
+                    "--length", str(chunk.length)], stdout=f)
+    return job.fileStore.writeGlobalFile(bed)
+
+def collate_chunks(job, finished_chunk_ids):
+    """Merge all the single-copy BED chunks into one large sorted BED file."""
+    finished_chunks = [job.fileStore.readGlobalFile(chunk) for chunk in finished_chunk_ids]
+    unsorted_bed = job.fileStore.getLocalTempFile()
+    with open(unsorted_bed, 'w') as f:
+        check_call(["cat"] + finished_chunks, stdout=f)
+
+    sorted_bed = job.fileStore.getLocalTempFile()
+    with open(sorted_bed, 'w') as f:
+        check_call(["bedtools", "sort", "-i", unsorted_bed], stdout=f)
+    return job.fileStore.writeGlobalFile(sorted_bed)
+
+# Luigi tasks
+
 class ExtractSingleCopyRegions(luigi.Task):
-    """Get a BED of single-copy regions from a hal file."""
+    """Get a BED of single-copy regions from a hal file.
+
+    Delegates to a toil pipeline to parallelize the process."""
     hal_file = luigi.Parameter()
     genome = luigi.Parameter()
+    chunk_size = luigi.IntParameter(default=500000)
 
     def output(self):
         return luigi.LocalTarget('singleCopyRegions-%s.bed' % self.genome)
 
     def run(self):
-        with self.output().open('w') as f:
-            check_call(["halSingleCopyRegionsExtract", self.hal_file, self.genome], stdout=f)
+        opts = Job.Runner.getDefaultOptions('./jobStore')
+        with Toil(opts) as toil:
+            result = toil.start(Job.wrapJobFn(extract_single_copy_regions_parallel,
+                                              os.path.abspath(self.hal_file), self.genome,
+                                              self.chunk_size))
+            toil.exportFile(result, 'file://' + os.path.abspath(self.output().path))
 
 class ApplySingleCopyFilter(luigi.Task):
     """Restrict the bed file to be only within single-copy regions."""
